6a05b47e67738bf9b9bc6aa2b2cbecad755acb12
cucumberframework <66793368+cucumberframework@users.noreply.github.com>
2023-07-11 19:48:57 +0530

    Added first commit in WDIO

---
+++ example.e2e.js
@@ -0,0 +1,15 @@
+import LoginPage from '/login.page.js'
+import SecurePage from '/secure.page.js'
+
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        await LoginPage.open()
+
+        await LoginPage.login('tomsmith', 'SuperSecretPassword!')
+        await expect(SecurePage.flashAlert).toBeExisting()
+        await expect(SecurePage.flashAlert).toHaveTextContaining(
+            'You logged into a secure area!')
+    })
+})
+
+
