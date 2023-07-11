e70218968164be64544e17bd86f461bee2e5562b
cucumberframework <66793368+cucumberframework@users.noreply.github.com>
2023-07-11 20:02:02 +0530

    Pushed WDIO project to GIT master

---
+++ javascriptBasicsCode/basics.js
@@ -0,0 +1,27 @@
+console.log("Hello World")
+
+let a="Rahul"
+console.log(a)
+console.log(typeof(a))
+
+let b=218.66
+console.log(b)
+console.log(typeof(b))
+
+let d=true
+console.log(d)
+console.log(typeof(d))
+
+
+
+
+//difference between var and let keyword
+//we cannot redeclare variable with let keyword but possible with var
+
+//single line comment 
+
+
+/* 
+
+multi line comment 
+*/
