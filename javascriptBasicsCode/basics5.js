4cea6321379612710ae6561e6c1f86fad18dbf41
cucumberframework <66793368+cucumberframework@users.noreply.github.com>
2023-07-18 17:51:21 +0530

    Added WDIO tutorials

---
+++ javascriptBasicsCode/basics5.js
@@ -0,0 +1,20 @@
+let day='tuesday '
+
+//to get the length of the string 
+console.log("String length is -->"+day.length)
+
+//to take the substring of hte current string 
+let subStringOfCurrentString= day.slice(2,4)
+console.log(subStringOfCurrentString)
+
+//get particular character from the string 
+console.log("Char present at index3 is="+day[3])
+
+//split the string based on the character 
+let splittedString= day.split("s") 
+console.log(splittedString)
+
+let date1="10"
+let date2="15"
+let dateDifference= parseInt(date2)-parseInt(date1)
+console.log(dateDifference)
