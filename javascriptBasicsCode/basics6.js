f103e87f755b11790a4d7769bd339ff093001320
cucumberframework <66793368+cucumberframework@users.noreply.github.com>
2023-07-18 20:03:30 +0530

    Added code for tutrial 21

--- javascriptBasicsCode/basics6.js
+++ javascriptBasicsCode/basics6.js
@@ -1,7 +1,12 @@
-let person={
+let person = {
 
-    firstName: 'Rahul', 
-    lastName: 'Barapatre'
+    firstName: 'Rahul',
+    lastName: 'Barapatre',
+    gender: 'Male',
+    Address: 'Tent Line',
+    fullName: function () {
+        console.log(this.firstName + this.lastName)
+    }
 }
 
 console.log(person.firstName)
@@ -10,13 +15,24 @@ console.log(person.firstName)
 console.log(person['firstName'])
 
 //Changing the value from the object 
-person.firstName='Megha'
+person.firstName = 'Megha'
 console.log(person.firstName)
 
 //Adding new values in the object 
-person.gender= 'Male'
+person.gender = 'Male'
 console.log(person)
 
 //deleting the calue from the object 
 delete person.gender
 console.log(person)
+
+//code for tutorial 21 
+console.log('gender' in person)             //returns false if the property does NOT exist in the object 
+
+//printing all key and values pair from the object 
+
+for (let key in person) {
+    console.log("Key is :" + key + " and value is :" + person[key])
+}
+
+console.log(person.fullName())
